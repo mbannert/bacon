f43d67bdf336bd276e2824a732e095192a92bbde
mbannert <matthias.bannert@gmail.com>
2017-05-13 00:26:03 +0200

    notes on version control, template DESCRIPTION

--- inst/applied.R
+++ inst/applied.R
@@ -1,4 +1,105 @@
+# take a way for the book: 
+# need a guideline: what to with an unknown dataset
+# guide to descriptive statistics - cheat sheet / reference / guide
+# 
+
+
 library(fivethirtyeight)
+d <- data(package="fivethirtyeight")
+d_name_538 <- d$results[,"Item"]
+
+dlist <- lapply(d_name_538,get)
+names(dlist) <- d_name_538
+
+head(dlist$unisex_names)
+
+
+library(googleVis)
+# only needed vor markdown... 
+# op <- options(gvis.plot.tag='chart')
+
+Geo <- gvisGeoChart(drinks, locationvar='country',
+                    colorvar='beer_servings', 
+                    options=list(height=300, width=750)) 
+Tbl <- gvisTable(drinks, options=list(height=300, width=200))
+plot(Geo)
+plot(gvisMerge(Geo, Tbl, horizontal=TRUE))
+
+
+
+
+lapply(dlist,function(x) c(nrow(x),ncol(x)))
+lapply(dlist,rowsAndCols)
+
+summaries <- lapply(dlist,summary)
+
+write.csv2(trump,
+           file="data/trump.csv")
+
+lapply(names(dlist),function(x){
+  ds <- get(x)
+  fname <- paste("data/",x,".csv",sep = "")
+  write.csv2(ds,file = fname)
+})
+
+
+
+
+trump <- dlist$trump_news
+rowsAndCols(trump)
+lapply(dlist,rowsAndCols)
+
+
+
+
+
+
+
+
+
+lapply(dlist,nrow)
+
+
+
+
+
+
+nameDerFunktion <- function(datensatz){
+  
+}
+
+
+meineFunktion(trump)
+dim(trump)
+
+
+lapply(dlist,dim)
+
+
+
+sapply(dlist,nrow)
+sapply(dlist,ncol)
+
+
+
+
+
+
+
+
+
+
+trump <- get("trump_news")
+
+
+
+
+
+
+
+
+
+
 
 # let's explore all of 538's datasets
 d <- data(package = "fivethirtyeight")
